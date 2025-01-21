8d9c80c645adb3a471e270608426b15606ea5307
Kauan Fonseca <131304556+COWANBAS@users.noreply.github.com>
2025-01-21 09:59:29 -0300

    Update Bloquear.Rastreadores.js

--- Bloquear.Rastreadores.js
+++ Bloquear.Rastreadores.js
@@ -3,7 +3,7 @@
 // @description   Bloquear rastreadores de dados pessoais em qualquer site, permitindo a pesquisa no Google
 // @namespace     CowanBlock
 // @license       CowBas
-// @version       2.0
+// @version       1.0
 // @author        Cowanbas
 // @match         *://*/*
 // @run-at        document-start
@@ -24,9 +24,7 @@
       let script = scriptTags[i];
       const src = script.getAttribute('src');
       // Bloqueia links de scripts de rastreadores conhecidos
-      const blockedDomains = ['google-analytics.com', 'facebook.com', 'doubleclick.net', 'adsense.com',
-        'googletagmanager.com', 'twitter.com', 'amazon-adsystem.com', 'yahoo.com',
-        'bing.com', 'quantserve.com', 'scorecardresearch.com', 'adnxs.com'];
+      const blockedDomains = ['google-analytics.com', 'facebook.com', 'doubleclick.net'];
       if (blockedDomains.some(domain => src.includes(domain))) {
         script.parentNode.removeChild(script);
       }
@@ -49,8 +47,7 @@
         continue; // Permite o iframe da pesquisa
       }
       // Bloqueia links de iframes de rastreadores conhecidos
-      const blockedDomains = ['youtube.com', 'vimeo.com', 'google.com', 'facebook.com', 'instagram.com',
-        'twitter.com', 'linkedin.com', 'tiktok.com', 'snapchat.com'];
+      const blockedDomains = ['youtube.com', 'vimeo.com', 'google.com'];
       if (blockedDomains.some(domain => src.includes(domain))) {
         iframe.parentNode.removeChild(iframe);
       }
