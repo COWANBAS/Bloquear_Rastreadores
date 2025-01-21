8a1b8a66e172359f04fa761554acbf25796de979
Kauan Fonseca <131304556+COWANBAS@users.noreply.github.com>
2025-01-21 09:21:04 -0300

    Update Bloquear.Rastreadores.js

--- Bloquear.Rastreadores.js
+++ Bloquear.Rastreadores.js
@@ -24,7 +24,9 @@
       let script = scriptTags[i];
       const src = script.getAttribute('src');
       // Bloqueia links de scripts de rastreadores conhecidos
-      const blockedDomains = ['google-analytics.com', 'facebook.com', 'doubleclick.net'];
+      const blockedDomains = ['google-analytics.com', 'facebook.com', 'doubleclick.net', 'adsense.com',
+        'googletagmanager.com', 'twitter.com', 'amazon-adsystem.com', 'yahoo.com',
+        'bing.com', 'quantserve.com', 'scorecardresearch.com', 'adnxs.com'];
       if (blockedDomains.some(domain => src.includes(domain))) {
         script.parentNode.removeChild(script);
       }
@@ -47,7 +49,8 @@
         continue; // Permite o iframe da pesquisa
       }
       // Bloqueia links de iframes de rastreadores conhecidos
-      const blockedDomains = ['youtube.com', 'vimeo.com', 'google.com'];
+      const blockedDomains = ['youtube.com', 'vimeo.com', 'google.com', 'facebook.com', 'instagram.com',
+        'twitter.com', 'linkedin.com', 'tiktok.com', 'snapchat.com'];
       if (blockedDomains.some(domain => src.includes(domain))) {
         iframe.parentNode.removeChild(iframe);
       }
