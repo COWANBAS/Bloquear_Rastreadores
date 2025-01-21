b84b91f8a35bb11e0ebceae856a7e715da20c6b8
Kauan Fonseca <131304556+COWANBAS@users.noreply.github.com>
2025-01-21 09:50:28 -0300

    Update Bloquear.Rastreadores.js

--- Bloquear.Rastreadores.js
+++ Bloquear.Rastreadores.js
@@ -1,4 +1,4 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name          Bloqueio de Rastreadores
 // @description   Bloquear rastreadores de dados pessoais em qualquer site, permitindo a pesquisa no Google
 // @namespace     CowanBlock
@@ -25,35 +25,8 @@
       const src = script.getAttribute('src');
       // Bloqueia links de scripts de rastreadores conhecidos
       const blockedDomains = ['google-analytics.com', 'facebook.com', 'doubleclick.net', 'adsense.com',
-    'googletagmanager.com', 'twitter.com', 'amazon-adsystem.com', 'yahoo.com',
-    'bing.com', 'quantserve.com', 'scorecardresearch.com', 'adnxs.com',
-    'newrelic.com', 'mixpanel.com', 'optimizely.com', 'hotjar.com',
-    'crazyegg.com', 'clicktale.net', 'fullstory.com', 'kissmetrics.com',
-    'matomo.org', 'piwik.pro', 'statcounter.com', 'chartbeat.com',
-    'segment.com', 'heap.io', 'inspectlet.com', 'logrocket.com',
-    'flurry.com', 'appsflyer.com', 'branch.io', 'adjust.com',
-    'kochava.com', 'clevertap.com', 'moengage.com', 'onesignal.com',
-    'swrve.com', 'airship.com', 'applovin.com', 'tapjoy.com',
-    'unity3d.com', 'vungle.com', 'ironsrc.com', 'inmobi.com',
-    'adroll.com', 'perfectaudience.com', 'criteo.com', 'outbrain.com',
-    'taboola.com', 'revcontent.com', 'zergnet.com', 'mgid.com',
-    'content.ad', 'plista.com', 'nativo.com', 'sharethrough.com',
-    'pubmatic.com', 'rubiconproject.com', 'openx.com', 'indexexchange.com',
-    'appnexus.com', 'smaato.com', 'spotx.tv', 'fyber.com',
-    'chartboost.com', 'adcolony.com', 'liftoff.io', 'taptica.com',
-    'mediamath.com', 'thetradedesk.com', 'xandr.com', 'verizonmedia.com',
-    'simplifi.com', 'voxmedia.com', 'sojern.com', 'adara.com',
-    'traveladnetwork.com', 'adzerk.com', 'gumgum.com', 'sublime.com',
-    'yieldmo.com', 'triplelift.com', 'teads.tv', 'tabmo.io',
-    'kargo.com', 'unruly.co', 'smartadserver.com', 'adform.com',
-    'bidtellect.com', 'connatix.com', 'adsymptotic.com', 'datonics.com',
-    'alphonso.tv', 'comscore.com', 'nielsen.com', 'experian.com',
-    'bluekai.com', 'liveramp.com', 'neustar.biz', 'oracle.com',
-    'quantcast.com', 'mopub.com', 'admob.com', 'doubleverify.com',
-    'integralads.com', 'whiteops.com', 'zvelo.com', 'shield.io',
-    'permutive.com', 'perimeterx.com', 'securiti.ai', 'trustarc.com',
-    'onetrust.com', 'privitar.com', 'bigid.com', 'osano.com',
-    'wirewheel.io', 'radware.com', 'cloudflare.com', 'akamai.com'];
+        'googletagmanager.com', 'twitter.com', 'amazon-adsystem.com', 'yahoo.com',
+        'bing.com', 'quantserve.com', 'scorecardresearch.com', 'adnxs.com'];
       if (blockedDomains.some(domain => src.includes(domain))) {
         script.parentNode.removeChild(script);
       }
